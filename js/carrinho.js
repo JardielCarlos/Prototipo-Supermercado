e846234ab384a789d104ee81d7e53320320bf09c
Jardiel <Jardiel.CarlosM@hotmail.com>
2022-06-17 18:12:04 -0300

    Update

--- js/carrinho.js
+++ js/carrinho.js
@@ -1,4 +1,11 @@
 var finalizarPedido = document.getElementById("btnFinalizar")
+var precos = document.querySelectorAll(".preco")
+var subtotal = document.getElementById("spanSubtotal")
+var finalizarPreco = document.getElementById("finalizarPreco")
+var finalizarTotal = document.getElementById("finalizarTotal")
+var dezporcentagem = document.getElementById("porcentagem")
+var totItens = document.getElementById("totItens")
+var totfinalizar = document.getElementById("totfinalizar")
 
 finalizarPedido.addEventListener("click", function(){
   finalizarPedido.textContent = 'Compra Realizada com Sucesso' 
@@ -8,5 +15,23 @@ finalizarPedido.addEventListener("click", function(){
 })
 
 
+var total = 0
+for(let i = 0; i < precos.length; i++){
+  let preco = precos[i].textContent
+  let valor = parseFloat(preco.replace("R$", ""))
+  total += valor
+  var arredondar = +(total.toFixed(2))
+  
 
+  subtotal.textContent = `R$ ${arredondar}`
+  finalizarPreco.textContent = `R$ ${arredondar}`
+  finalizarTotal.textContent = `R$ ${arredondar}`
+  
+  porcentagem = (10/100) * total
+  porcentagemtotal = total + porcentagem
+  let arredondado  = +(porcentagemtotal.toFixed(2))
+  dezporcentagem.innerHTML = `ou R$ ${arredondado} em até 8x <img src="img/credit-card.svg" alt="Imagem de um cartão de credito">`
+}
 
+totItens.textContent = `(${precos.length} itens)`
+totfinalizar.textContent = `${precos.length} Produtos`
